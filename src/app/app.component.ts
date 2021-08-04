bfcfcfea70081955d0870d430e948cc8cdc99df8
sridharbommena <smartboysridhar@gmail.com>
2021-08-04 12:46:30 +0530

    fist commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { post } from './models/post';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'job-portal';
+export class AppComponent implements OnInit {
+  
+  constructor() { }
+  posts:post[] = [];
+  dummyPost:post = Object.create(
+      { 
+        id: null,
+        heading: "do it now some dummy text",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lacus ipsum, elementum id enim non, finibus tempus est. Integer molestie, nisi ut bibendum cursus, eros est lobortis leo, vel commodo elit turpis vel arcu. Aenean tempus ex vitae gravida ultricies. Vestibulum finibus fermentum felis. Duis odio neque, iaculis non justo porttitor, pulvinar sollicitudin lacus. Morbi non dolor nunc. Phasellus semper id ante nec condimentum. Nam quis nunc varius, finibus ipsum sed, rutrum orci. Donec dui elit, porttitor facilisis lacus sit amet, bibendum porta tortor. Quisque ut condimentum orci. Sed condimentum tempor odio, eget mollis sem interdum in. Nulla vitae ultrices tellus, vel posuere mi. Maecenas id justo eget lorem egestas ornare. Nullam vitae dapibus quam. Nunc a nulla sit amet lectus aliquet semper ac vel sem.",
+        links:"https://apply_link",
+        img_url: "https://image_url",
+        original_apply_link: "https://orginal_apply_link",
+        date: "20-08-2021"
+      });
+
+  ngOnInit(): void {
+
+    for (let index = 0; index < 10; index++) {
+      this.posts.push(this.dummyPost);
+    }
+
+    console.log(this.posts);
+
+  }
+  
 }
---
+++ src/app/models/post.ts
@@ -0,0 +1,10 @@
+export interface post
+{
+    id:number;
+    heading:string;
+    description:string;
+    img_url: string;
+    links:string;
+    original_apply_link:string;
+    date:string;
+}
