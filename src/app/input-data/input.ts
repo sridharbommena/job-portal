05d8a192baf96b4b20fda21498dec82e571e6624
sridharbommena <smartboysridhar@gmail.com>
2021-08-04 15:27:26 +0530

    third commit

---
+++ src/app/input-data/input.ts
@@ -0,0 +1,96 @@
+import { post } from "../models/post";
+
+export const posts:post[] = [
+    {
+        id : 1,
+        semi_heading: "Capgemini Off Campus Drive",
+        heading: "Capgemini Off Campus Drive 2021 for SAP | B.E/B.Tech | 2019/2020 Batch | Across India",
+        description: "Capgemini scheduled to hire for the role of SAP – SD  and SAP – MM for B.E/ B.Tech graduates – 2019/ 2020 batch. The detailed eligibility and application process are given below.",
+        img_url: "https://www.freshersvoice.com/wp-content/uploads/2017/01/Capgemini-Off-Campus.png",
+        job_role: "SAP – SD",
+        qualification:	"B.E/B.Tech",
+        batch: "2019/2020",
+        experience: "1 – 2 years",
+        salary:	"Rs. 3 – 3.8 LPA",
+        job_location: "Bangalore/Chennai/Mumbai/Pune",
+        drive_location: null,
+        drive_date: null,
+        last_date: "ASAP",
+        detailed_eligibility: [
+            "B.E/ B.Tech (Any Specialization)",
+            "Minimum Marks 60% Graduation in 2019 & 2020",
+            "Min. Class XII Marks: 50%", 
+            "Min. Class X Marks: 50%",
+            "No Backlogs Accepted"
+        ],
+        professional_experience: [
+            "Domain experience of minimum 1 – 2 years in any of the following areas",
+            "Sale coordinators (back office customer service)",
+            "Warehouse or store keepers dealing with a different type of goods movement.",
+            "Manufacturing experience including raw material to finished product processing.",
+            "The business role involved sales order, delivery, shipment & billing document processing.",
+            "Basic Business process understating is required"
+        ],
+        skills: [
+            "Good communication skills – both oral & written – in English",
+            "Right attitude & temperament",
+            "Quick grasping power",
+            "Excellent written and oral communication skills",
+            "Team Player",
+            "Interacting with People",
+            "Flexibility in terms of technology adaption.",
+        ],
+        job_description:null,
+        apply_link: "https://www.firstnaukri.com/job-listings-SAP-SD-Engg-jobs-in-Bengaluru-Bangalore-Chennai-Mumbai-Pune-in-Capgemini-Technology-Services-India-Limited-280721600016?srchId=&srchRank=1&pageNo=1",
+
+    },
+    {
+        id : 2,
+        semi_heading: "Accenture Off Campus Event 2021",
+        heading: "Accenture Off Campus Drive 2021 | B.E/B.Tech/M.E/M.Tech/MCA/M.Sc/B.Sc/BCA | 2019/2020/2021 Batch | Across India",
+        description: "Accenture Solutions Pvt Ltd planned to conduct Off Campus Drive to hire B.E/B.Tech/M.E/M.Tech/MCA/M.Sc & B.Sc/BCA 2019,2020,2021 Batch graduates. The more details about Accenture off campus drive given below.",
+        img_url: "https://www.freshersvoice.com/wp-content/uploads/2017/01/Accenture-Off-Campus.png",
+        job_role: "Associate Software Engineer",
+        qualification:	"B.E/B.Tech/M.E/M.Tech/MCA/M.Sc",
+        batch: "2019/2020/2021",
+        experience: "0-1 Year",
+        salary:	"Rs 4.5 LPA",
+        job_location: "Bangalore, Hyderabad, Chennai, Mumbai, Pune, Gurugram",
+        drive_location:"Online(Virtual)",
+        drive_date:"Announced Later",
+        last_date: "10 August 2021",
+        detailed_eligibility: [
+            "Full time program in B.E. / B. Tech / M.E / M.Tech / MCA / M.Sc (All streams).",
+            "Year of passing: Graduates from 2019/2020/2021.",
+            "Backlogs: No active backlogs both during application and at the time of Onboarding.",
+            "Gap in Education: Maximum 1-year gap in education (post 10th standard till completion of graduation).",
+            "Applied for Accenture Candidate should not have appeared for Accenture Recruitment assessment / interview process in last three months",
+            "Work Permit: Should be an Indian Citizen. Bhutan & Nepal nationals can work in India without a need for obtaining a visa to work. All other foreign nationals require work visa or an OCI / PIO card to work in India. Bhutan & Nepal nationals would also need to submit a Citizenship certificate or a Govt. ID Card in the absence of other documents (PIO/OCI/Work permit/passport)"
+        ],
+        professional_experience: [
+            "Domain experience of minimum 1 – 2 years in any of the following areas",
+            "Sale coordinators (back office customer service)",
+            "Warehouse or store keepers dealing with a different type of goods movement.",
+            "Manufacturing experience including raw material to finished product processing.",
+            "The business role involved sales order, delivery, shipment & billing document processing.",
+            "Basic Business process understating is required"
+        ],
+        skills: [
+            "Good communication skills – both oral & written – in English",
+            "Right attitude & temperament",
+            "Quick grasping power",
+            "Excellent written and oral communication skills",
+            "Team Player",
+            "Interacting with People",
+            "Flexibility in terms of technology adaption.",
+        ],
+        job_description:[
+            "Design, build, test, assemble, support, and configure the application using business requirements",
+            "Understand business drivers that will impact performance and deliver software to those expectations",
+            "Bake technology trends into solutions; Participate in the development of automation solutions, new functionality, and technologies and integrate them into existing solutions",
+            "Responsible for incident resolution and support extended to customers through voice interaction/email/chat/remote support"
+        ],
+        apply_link: "https://indiacampus.accenture.com/register/accenture/ndirl1/apply/?event=3233",
+
+    }
+]
--- src/app/models/post.ts
+++ src/app/models/post.ts
@@ -1,10 +1,29 @@
 export interface post
 {
-    id:number;
-    heading:string;
-    description:string;
-    img_url: string;
-    links:string;
-    original_apply_link:string;
-    date:string;
+    // id:number;
+    // heading:string;
+    // description:string;
+    // img_url: string;
+    // links:string;
+    // original_apply_link:string;
+    // date:string;
+        id : number;
+        semi_heading: string;
+        heading: string;
+        description: string;
+        img_url: string;
+        job_role: string;
+        qualification:	string;
+        batch: string;
+        experience: string;
+        salary:	string;
+        job_location: string;
+        drive_location:string;
+        drive_date:string;
+        last_date:string;
+        detailed_eligibility: Array<string>;
+        professional_experience:Array<string>;
+        skills:Array<string>;
+        job_description: Array<string>;
+        apply_link: string;
 }
